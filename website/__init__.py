8f2c9518f41c4ffed92cefccfd24c84dd6cf6c79
danettp <17204@my.sanctamaria.school.nz>
2023-06-05 11:49:01 +1200

    SQLAlchemy database setup
    Initialized database with flask application and created database

--- website/__init__.py
+++ website/__init__.py
@@ -3,10 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__) # Create a Flask application instance
     app.config['SECRET_KEY'] = "helloworld"
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views # (Relative) import inside the python package
     from .auth import auth
@@ -14,4 +19,11 @@ def create_app():
     app.register_blueprint(views, url_prefix="/") # Register the views Blueprint with the application
     app.register_blueprint(auth, url_prefix="/")
     
+    create_database(app)
+    
     return app # Return the created application instance
+
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created database!")
