c97f2edf2247daab9e6fefffcdce95113e4651bf
danettp <17204@my.sanctamaria.school.nz>
2023-06-05 11:38:30 +1200

    HTTP Post Method
    allow signup and login to accept post request

--- website/auth.py
+++ website/auth.py
@@ -2,11 +2,11 @@ from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__) # Create a Blueprint instance named "auth"
 
-@auth.route("/login") # Define a route for the "/login" URL
+@auth.route("/login", methods=['GET', 'POST']) # Define a route for the "/login" URL
 def login():
     return render_template("login.html") # Render "login.html" template and return it as the response
 
-@auth.route("/sign-up") 
+@auth.route("/sign-up", methods=['GET', 'POST']) 
 def sign_up():
     return render_template("signup.html")
 
