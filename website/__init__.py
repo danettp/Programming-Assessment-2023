e6556e9298aed4ec360df4b99904bf8427c33a62
danettp <17204@my.sanctamaria.school.nz>
2023-06-05 12:06:21 +1200

    Flask login setup
    make sure database is created and working

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__) # Create a Flask application instance
     app.config['SECRET_KEY'] = "helloworld"
-    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     
     from .views import views # (Relative) import inside the python package
@@ -23,9 +23,18 @@ def create_app():
     
     create_database(app)
     
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login" # Redirects to Login page
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return app # Return the created application instance
 
 def create_database(app):
     if not path.exists("website/" + DB_NAME):
-        db.create_all(app=app)
+        with app.app_context():
+            db.create_all()
         print("Created database!")
