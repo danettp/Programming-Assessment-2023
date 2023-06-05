8121a94b4b361fb2fbb6b481654f0d171dcd84ff
danettp <17204@my.sanctamaria.school.nz>
2023-06-05 12:25:33 +1200

    Logging in Users
    storing data of user sign ups

--- website/auth.py
+++ website/auth.py
@@ -1,29 +1,43 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__) # Create a Blueprint instance named "auth"
 
-@auth.route("/login", methods=['GET', 'POST']) # Define a route for the "/login" URL
+
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html") # Render "login.html" template and return it as the response
 
 
-@auth.route("/sign-up", methods=['GET', 'POST']) 
+@auth.route("/sign-up", methods=['GET', 'POST']) # Define a route for the "/login" URL
 def sign_up():
     if request.method == 'POST':
         email = request.form.get("email")
         username = request.form.get("username")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
-        
+
         email_exists = User.query.filter_by(email=email).first()
         username_exists = User.query.filter_by(username=username).first()
-        
+
         if email_exists:
             flash('Email is already in use.', category='error')
         elif username_exists:
@@ -41,12 +55,15 @@ def sign_up():
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(new_user, remember=True)
+            login_user(new_user, remember=True) # Function must be called in current user session to access page
             flash('User created!')
             return redirect(url_for('views.home'))
-    
+
     return render_template("signup.html")
 
-@auth.route("/logout") 
+
+@auth.route("/logout")
+@login_required # User can only access page if logged in
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))
