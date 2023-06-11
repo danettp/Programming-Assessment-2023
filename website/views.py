6da344334f42c87e01749d2c91686d06f3feeda0
danettp <17204@my.sanctamaria.school.nz>
2023-06-12 10:36:57 +1200

    Delete comment view
    Created comment view and able to actually delete comment

--- website/views.py
+++ website/views.py
@@ -59,7 +59,7 @@ def posts(username):
         return redirect(url_for('views.home'))
 
     posts = user.posts
-    return render_template("posts.html", user=current_user, posts=posts, username=username) 
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
 
 
 @views.route("/create-comment/<post_id>", methods=['POST'])
@@ -78,5 +78,21 @@ def create_comment(post_id):
             db.session.commit()
         else:
             flash('Post does not exist.', category='error')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
 
     return redirect(url_for('views.home'))
